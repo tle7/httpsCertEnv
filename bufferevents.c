4f7b9b41372d8a0344e6b66bc2aaa4e1c2b28b0b
Emily Marx <emilykmarx@gmail.com>
2019-05-26 05:38:26 +0000

    works for 2 IPs at once

--- bufferevents.c
+++ bufferevents.c
@@ -20,6 +20,11 @@
 
 // TODO: lots of frees, error-catching probably 
 
+int verify(X509 *leaf_cert) {
+    int validity = X509_check_ca(leaf_cert);
+    return validity; // returns error code enum I think
+}
+
 void CAs(X509 *leaf_cert) {
     char *subject = X509_NAME_oneline(X509_get_subject_name(leaf_cert), NULL, 0); 
     // use blog code to parse fields (e.g. location)
@@ -31,41 +36,44 @@ void CAs(X509 *leaf_cert) {
 
 void eventcb(struct bufferevent *bev, short events, void *ptr) {
     printf("in cb\n");
-    if (events & BEV_EVENT_CONNECTED) { // this may only be for the regular bufferevent_socket_connect
+    if (events & BEV_EVENT_CONNECTED) { // does this happen even if handshake fails?  
     /* We're connected: start reading/writing TODO: where? */
         printf("connected!!!\n");
+        SSL *ssl = bufferevent_openssl_get_ssl(bev);
+        X509 *leaf_cert = SSL_get_peer_certificate(ssl); 
+        // if returns NULL, count that has handshake failure?
+        STACK_OF(X509) *chain = SSL_get_peer_cert_chain(ssl); // this may be NULL: If so, push the cert?
+        verify(leaf_cert);
+        CAs(leaf_cert);
 
     } else if (events & BEV_EVENT_ERROR) {
     /* An error occured while connecting. */
-        printf("error connecting :(\n");
-    }   
-    SSL *ssl = bufferevent_openssl_get_ssl(bev);
-    X509 *leaf_cert = SSL_get_peer_certificate(ssl);
-    STACK_OF(X509) *chain = SSL_get_peer_cert_chain(ssl); // this may be NULL: If so, push the cert?
-    CAs(leaf_cert);
+        printf("error connecting in cb\n"); // count this as handshake failure too?
+        unsigned long err = bufferevent_get_openssl_error(bev);
+    }  
+    bufferevent_free(bev); // closes socket since close_on_free is set
+    //SSL_CTX_free(ssl_context); // TODO: should do this
+
 }
 
-struct bufferevent *handshake() {
-    // every application will have an eventbase. It keeps track of all pending and active events, and notifies your application of the active ones
-    // create event base
-    struct event_base *eb = event_base_new();
 
-    // setup socket
-    struct sockaddr_in sin;
-    memset(&sin, 0, sizeof(sin));
-    sin.sin_family = AF_INET;
-    sin.sin_addr.s_addr = inet_addr("192.30.255.113"); // github 
-    sin.sin_port = htons(443);
-
-    // get initial SSL*
+void handshake(char *ip, struct event_base *base) {
+     // get initial SSL*: only works if each one has their own
     SSL_CTX *ssl_context = SSL_CTX_new(TLS_client_method()); 
     // specifying version is deprecated...can't do TLS 1.3?
     SSL *ssl = SSL_new(ssl_context); // have to start somewhere...
-    struct bufferevent *bev = bufferevent_openssl_socket_new(eb, -1, ssl, 
+    // setup socket
+    struct sockaddr_in sin;
+    memset(&sin, 0, sizeof(sin)); // perhaps pre-compute this: array of sockaddrs instead of IPs?
+    sin.sin_family = AF_INET;
+    sin.sin_addr.s_addr = inet_addr(ip); // github 
+    sin.sin_port = htons(443);
+
+    struct bufferevent *bev = bufferevent_openssl_socket_new(base, -1, ssl, 
 			BUFFEREVENT_SSL_CONNECTING,
-			BEV_OPT_CLOSE_ON_FREE | BEV_OPT_DEFER_CALLBACKS ); 
+			BEV_OPT_CLOSE_ON_FREE | BEV_OPT_DEFER_CALLBACKS); 
     if (!bev) {
-        printf("error in openssl_socket_new\n");
+        printf("error in openssl_socket_new with ip %s\n", ip);
     }
    
     // set callback
@@ -75,28 +83,29 @@ struct bufferevent *handshake() {
     int fd = bufferevent_socket_connect(bev, (struct sockaddr *)&sin, sizeof(sin));
     if (fd == -1) { // returns 0 on success 
         /* Error starting connection */ 
-        printf("error connecting to socket\n");
-        bufferevent_free(bev);
-        return NULL;
+        printf("error connecting to socket with ip %s\n", ip);
     } else {
-        printf("connected to socket\n");
+        printf("connected to socket with ip %s\n", ip);
     }
-    //bufferevent_setfd(bev, fd); // TODO
-    event_base_dispatch(eb);
-    // event_base_loop(eb);  // runs until no more events, or call break/edit
-    return bev;
 }
 
-
-
-int valid(X509 *leaf_cert) {
-    int validity = X509_check_ca(leaf_cert);
-    return validity; // returns error code enum I think
-}
 
 int main() {
     //OpenSSL_add_all_algorithms(); // maybe only needed for openssl 1.0
-    struct bufferevent *bev = handshake();
-     
+    
+    char *ips[] = {"192.30.255.113","143.204.129.163" }; // github, slack (google requires SNI)
+    //char *ips[] = {"192.30.255.113"}; 
+    size_t n_ips = sizeof(ips)/sizeof(ips[0]);
+
+    // create event base
+    struct event_base *base = event_base_new();
+
+    for (int i = 0; i < n_ips; i++) {
+        handshake(ips[i], base);
+    }
+        //bufferevent_setfd(bev, fd); // TODO
+    event_base_dispatch(base);
+    // event_base_loop(base);  // runs until no more events, or call break/edit
+    event_base_free(base);
     return 0;
 }
